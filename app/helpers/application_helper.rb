cfa0aa011b311a1d77e041131cce2f58359464a5
sighmon <simon.loffler@gmail.com>
2015-07-05 14:41:32 +0930

    Fixed passenger relationship.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,9 +8,22 @@ module ApplicationHelper
 		return return_controller
 	end
 
-	def voyage_start_stop(voyage)
+	def voyage_start(voyage)
 		stops_sorted = voyage.stops.sort_by(&:arrival)
-		return stops_sorted.first.port.name + " to " + stops_sorted.last.port.name
+		return stops_sorted.first
+	end
+
+	def voyage_stop(voyage)
+		stops_sorted = voyage.stops.sort_by(&:arrival)
+		return stops_sorted.last
+	end
+
+	def embarked_ports_for_passenger(passenger)
+		EmbarkedPassenger.where(passenger_id: passenger.id)
+	end
+
+	def disembarked_ports_for_passenger(passenger)
+		DisembarkedPassenger.where(passenger_id: passenger.id)
 	end
 
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,10 +1,12 @@
 require 'elasticsearch/model'
 
 class Passenger < ActiveRecord::Base
-	belongs_to :embarked, class_name: 'Stop'
-  belongs_to :disembarked, class_name: 'Stop'
-  accepts_nested_attributes_for :embarked
-  accepts_nested_attributes_for :disembarked
+	belongs_to :embarked_passengers
+  belongs_to :disembarked_passengers
+  accepts_nested_attributes_for :embarked_passengers
+  accepts_nested_attributes_for :disembarked_passengers
+
+  delegate :stop, to: :embarked_passengers, allow_nil: true
 
   include Elasticsearch::Model
   include Elasticsearch::Model::Callbacks
